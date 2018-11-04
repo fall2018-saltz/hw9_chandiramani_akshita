036b1d7b968b6bb68b972a9e837b30454843626b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-04 16:23:10 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -7,5 +7,9 @@ clean_data$CustSatBuckets <- replicate(length(clean_data$overallCustSat), "Avera
 clean_data$CustSatBuckets[clean_data$overallCustSat > 7] <- "High"
 clean_data$CustSatBuckets[clean_data$overallCustSat < 7] <- "Low"
 
+clean_data$hotelSizeBuckets <- replicate(length(clean_data$hotelSize), "Average")
+clean_data$hotelSizeBuckets[clean_data$hotelSize > 7] <- "High"
+clean_data$hotelSizeBuckets[clean_data$hotelSize < 7] <- "Low"
+
 str(clean_data)
 
