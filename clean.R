60edd661c1357f517510b94b721ffe192c6f3ac1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-04 16:21:39 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -4,3 +4,8 @@ JsonData<-fromJSON("hotelSurveyBarriot.json")
 clean_data <- data.frame(JsonData)
 
 str(clean_data)
+
+clean_data$CustSatBuckets <- replicate(length(clean_data$overallCustSat), "Average")
+clean_data$CustSatBuckets[clean_data$overallCustSat > 7] <- "High"
+clean_data$CustSatBuckets[clean_data$overallCustSat < 7] <- "Low"
+
