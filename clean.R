5c5a9ab1d860cda912ef7b5fa4cb356b6bbec2c7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-04 17:05:16 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,50 +1,50 @@
 
 library(jsonlite)
 JsonData<-fromJSON("hotelSurveyBarriot.json")
-clean_data <- data.frame(JsonData)
+hotelSurvey <- data.frame(JsonData)
 
-clean_data$CustSatBuckets <- replicate(length(clean_data$overallCustSat), "Average")
-clean_data$CustSatBuckets[clean_data$overallCustSat > 7] <- "High"
-clean_data$CustSatBuckets[clean_data$overallCustSat < 7] <- "Low"
-str(clean_data)
+hotelSurvey$CustSatBuckets <- replicate(length(hotelSurvey$overallCustSat), "Average")
+hotelSurvey$CustSatBuckets[hotelSurvey$overallCustSat > 7] <- "High"
+hotelSurvey$CustSatBuckets[hotelSurvey$overallCustSat < 7] <- "Low"
+str(hotelSurvey)
 
-clean_data$checkInSatBuckets <- replicate(length(clean_data$checkInSat), "Average")
-clean_data$checkInSatBuckets[clean_data$checkInSat > 7] <- "High"
-clean_data$checkInSatBuckets[clean_data$checkInSat < 7] <- "Low"
+hotelSurvey$checkInSatBuckets <- replicate(length(hotelSurvey$checkInSat), "Average")
+hotelSurvey$checkInSatBuckets[hotelSurvey$checkInSat > 7] <- "High"
+hotelSurvey$checkInSatBuckets[hotelSurvey$checkInSat < 7] <- "Low"
 
-clean_data$hotelCleanBuckets <- replicate(length(clean_data$hotelClean), "Average")
-clean_data$hotelCleanBuckets[clean_data$hotelClean > 7] <- "High"
-clean_data$hotelCleanBuckets[clean_data$hotelClean < 7] <- "Low"
+hotelSurvey$hotelCleanBuckets <- replicate(length(hotelSurvey$hotelClean), "Average")
+hotelSurvey$hotelCleanBuckets[hotelSurvey$hotelClean > 7] <- "High"
+hotelSurvey$hotelCleanBuckets[hotelSurvey$hotelClean < 7] <- "Low"
 
-clean_data$hotelFriendlyBuckets <- replicate(length(clean_data$hotelFriendly), "Average")
-clean_data$hotelFriendlyBuckets[clean_data$hotelFriendly > 7] <- "High"
-clean_data$hotelFriendlyBuckets[clean_data$hotelFriendly < 7] <- "Low"
+hotelSurvey$hotelFriendlyBuckets <- replicate(length(hotelSurvey$hotelFriendly), "Average")
+hotelSurvey$hotelFriendlyBuckets[hotelSurvey$hotelFriendly > 7] <- "High"
+hotelSurvey$hotelFriendlyBuckets[hotelSurvey$hotelFriendly < 7] <- "Low"
 
-q <- quantile(clean_data$hotelSize, c(0.4, 0.6))
-clean_data$hotelSizeBuckets <- replicate(length(clean_data$hotelSize), "Average")
-clean_data$hotelSizeBuckets[clean_data$hotelSize <= q[1]] <- "Low"
-clean_data$hotelSizeBuckets[clean_data$hotelSize > q[2]] <- "High"
+q <- quantile(hotelSurvey$hotelSize, c(0.4, 0.6))
+hotelSurvey$hotelSizeBuckets <- replicate(length(hotelSurvey$hotelSize), "Average")
+hotelSurvey$hotelSizeBuckets[hotelSurvey$hotelSize <= q[1]] <- "Low"
+hotelSurvey$hotelSizeBuckets[hotelSurvey$hotelSize > q[2]] <- "High"
 
-q <- quantile(clean_data$guestAge, c(0.4, 0.6))
-clean_data$guestAgeBuckets <- replicate(length(clean_data$guestAge), "Average")
-clean_data$guestAgeBuckets[clean_data$guestAge <= q[1]] <- "Low"
-clean_data$guestAgeBuckets[clean_data$guestAge > q[2]] <- "High"
+q <- quantile(hotelSurvey$guestAge, c(0.4, 0.6))
+hotelSurvey$guestAgeBuckets <- replicate(length(hotelSurvey$guestAge), "Average")
+hotelSurvey$guestAgeBuckets[hotelSurvey$guestAge <= q[1]] <- "Low"
+hotelSurvey$guestAgeBuckets[hotelSurvey$guestAge > q[2]] <- "High"
 
-q <- quantile(clean_data$lengthOfStay, c(0.4, 0.6))
-clean_data$lengthOfStayBuckets <- replicate(length(clean_data$lengthOfStay), "Average")
-clean_data$lengthOfStayBuckets[clean_data$lengthOfStay <= q[1]] <- "Low"
-clean_data$lengthOfStayBuckets[clean_data$lengthOfStay > q[2]] <- "High"
+q <- quantile(hotelSurvey$lengthOfStay, c(0.4, 0.6))
+hotelSurvey$lengthOfStayBuckets <- replicate(length(hotelSurvey$lengthOfStay), "Average")
+hotelSurvey$lengthOfStayBuckets[hotelSurvey$lengthOfStay <= q[1]] <- "Low"
+hotelSurvey$lengthOfStayBuckets[hotelSurvey$lengthOfStay > q[2]] <- "High"
 
-q <- quantile(clean_data$whenBookedTrip, c(0.4, 0.6))
-clean_data$whenBookedTripBuckets <- replicate(length(clean_data$whenBookedTrip), "Average")
-clean_data$whenBookedTripBuckets[clean_data$whenBookedTrip <= q[1]] <- "Low"
-clean_data$whenBookedTripBuckets[clean_data$whenBookedTrip > q[2]] <- "High"
+q <- quantile(hotelSurvey$whenBookedTrip, c(0.4, 0.6))
+hotelSurvey$whenBookedTripBuckets <- replicate(length(hotelSurvey$whenBookedTrip), "Average")
+hotelSurvey$whenBookedTripBuckets[hotelSurvey$whenBookedTrip <= q[1]] <- "Low"
+hotelSurvey$whenBookedTripBuckets[hotelSurvey$whenBookedTrip > q[2]] <- "High"
 
-str(clean_data)
+str(hotelSurvey)
 
-table(clean_data$guestAgeBuckets)
-prop.table(table(clean_data$guestAgeBuckets, dnn = "Percentage in different age groups"))
-table(clean_data$hotelFriendlyBuckets)
-prop.table(table(clean_data$hotelFriendlyBuckets, dnn = "Percentage in different hotel friendly buckets"))
+table(hotelSurvey$guestAgeBuckets)
+prop.table(table(hotelSurvey$guestAgeBuckets, dnn = "Percentage in different age groups"))
+table(hotelSurvey$hotelFriendlyBuckets)
+prop.table(table(hotelSurvey$hotelFriendlyBuckets, dnn = "Percentage in different hotel friendly buckets"))
 
-prop.table(table(clean_data$hotelFriendlyBuckets, clean_data$CustSatBuckets, dnn = c("hotelFriendly", "Customer Satisfaction")))
+prop.table(table(hotelSurvey$hotelFriendlyBuckets, hotelSurvey$CustSatBuckets, dnn = c("hotelFriendly", "Customer Satisfaction")))
